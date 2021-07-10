3c0d36ad349517d159a4c1626f4934aaa23615cf
VrutikShah <44443648+VrutikShah@users.noreply.github.com>
2021-07-11 04:07:31 +0530

    smooth 3 axis flight

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import math
 import numpy as np
 import pickle
 import csv
+import time
 # from aux_functions import *
 # TF Debug message suppressed
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -82,51 +83,51 @@ def generate_json(cfg):
 
     return flag
 
-def getObservation(client : airsim.MultirotorClient):
-        responses = client.simGetImages([
-            airsim.ImageRequest("0", airsim.ImageType.Scene, False, False),
-            airsim.ImageRequest("1", airsim.ImageType.Segmentation, False, False),
-            airsim.ImageRequest("2", airsim.ImageType.DepthPlanner, True, False)
-        ])
+# def getObservation(client : airsim.MultirotorClient):
+#         responses = client.simGetImages([
+#             airsim.ImageRequest("0", airsim.ImageType.Scene, False, False),
+#             airsim.ImageRequest("1", airsim.ImageType.Segmentation, False, False),
+#             airsim.ImageRequest("2", airsim.ImageType.DepthPlanner, True, False)
+#         ])
         
-        rgbImg = responses[0]
-        segImg = responses[1]
-        depthImg = responses[2]
-        depth = np.array(depthImg.image_data_float).reshape(depthImg.height, depthImg.width)
+#         rgbImg = responses[0]
+#         segImg = responses[1]
+#         depthImg = responses[2]
+#         depth = np.array(depthImg.image_data_float).reshape(depthImg.height, depthImg.width)
 
-        depth = np.clip(depth, 0, 50)/50
+#         depth = np.clip(depth, 0, 50)/50
         
-        depth = 1 - depth
+#         depth = 1 - depth
 
-        seg1d = np.fromstring(segImg.image_data_uint8, dtype=np.uint8)
-        seg = seg1d.reshape(segImg.height, segImg.width, 3)
+#         seg1d = np.fromstring(segImg.image_data_uint8, dtype=np.uint8)
+#         seg = seg1d.reshape(segImg.height, segImg.width, 3)
 
-        rgb1d = np.fromstring(rgbImg.image_data_uint8, dtype=np.uint8)
-        rgb = rgb1d.reshape(rgbImg.height, rgbImg.width, 3)
+#         rgb1d = np.fromstring(rgbImg.image_data_uint8, dtype=np.uint8)
+#         rgb = rgb1d.reshape(rgbImg.height, rgbImg.width, 3)
 
-        rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
+#         rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
 
-        observation = {}
-        observation['droneState'] = client.getMultirotorState()
-        observation["collision"] = client.simGetCollisionInfo().has_collided
-
-        
-
-        observation['depth'] = depth
-        observation['fpv'] = cv2.resize(rgb, (256, 256))
-
-        position = observation['droneState'].kinematics_estimated.position
-
-
-        observation['quadLocation'] =  [
-            position.x_val,
-            position.y_val,
-            position.z_val
-        ]
+#         observation = {}
+#         observation['droneState'] = client.getMultirotorState()
+#         observation["collision"] = client.simGetCollisionInfo().has_collided
 
         
+
+#         observation['depth'] = depth
+#         observation['fpv'] = cv2.resize(rgb, (256, 256))
+
+#         position = observation['droneState'].kinematics_estimated.position
+
+
+#         observation['quadLocation'] =  [
+#             position.x_val,
+#             position.y_val,
+#             position.z_val
+#         ]
+
         
-        return observation
+        
+#         return observation
 
 
 def do_action(action):
@@ -141,6 +142,7 @@ def do_action(action):
     VY_vehicle = action[1] 
     VZ_world =  action[2]
     yaw = action[3]
+    ya = action[4]
 
     VX_world = VX_vehicle * np.cos(yaw) - VY_vehicle * np.sin(yaw)
     VY_world = VX_vehicle * np.sin(yaw) + VY_vehicle * np.cos(yaw)
@@ -149,11 +151,12 @@ def do_action(action):
     # print(VX_vehicle, VY_vehicle, VZ_world, action)
 
 
-    client.moveByVelocityAsync(VX_world, VY_world, VZ_world, 1)
+    client.moveByVelocityAsync(VX_world, VY_world, VZ_world, 1,drivetrain=0,yaw_mode=ya)
     
 
 def getEuler(quat):
     # print(quat)
+    quat = quat.orientation
     w,x,y,z = quat.w_val,quat.x_val,quat.y_val,quat.z_val
     yaw = math.atan2(2*(w*z+x*y),1-2*(y**2+z**2))
     return yaw*180/math.pi
@@ -182,22 +185,27 @@ if __name__ == '__main__':
             dataset = []
             count = 0
             ya = YawMode()
+            ya.is_rate = True
             to_record = False
-            yaw_rate = 10
+            yaw_rate = 15
             font = cv2.FONT_HERSHEY_SIMPLEX
             org = (50, 50)
             fontScale = 1
             color = (255, 0, 0)
             thickness = 2
+            nexus = 0
             # quat = drone_state.kinematics_estimated.orientation
+            
             while True:
                 # client.moveByVelocityZAsync(0.01, 0, 0, 0.01)
                 # obs = getObservation(client)
 
                 image = get_MonocularImageRGB(client, name)
                 image2 = image
-                # angle = getEuler(quat)
-                text = str(angle)
+                quat = client.getImuData()
+                # print(quat.angular_velocity.z_val)
+                angle = getEuler(quat)
+                # text = str(round(angle,2))
                 # image = cv2.putText(image, text, org, font, 
                 #                 fontScale, color, thickness, cv2.LINE_AA)
                 count+=1
@@ -206,9 +214,9 @@ if __name__ == '__main__':
                 if k == ord('r'):
                     to_record = not to_record
                 if to_record:
-                    if count%10==0:
+                    if count%2==0:
                         # to save
-                        imname = 'img' + str(count//10) + '.jpg'
+                        imname = 'img' + str(count//2) + '.jpg'
                         cv2.imwrite('dataset/'+imname,image2)
                         dic = [vx,vy,vz,ya.yaw_or_rate]
                         dataset.append(dic)
@@ -218,7 +226,7 @@ if __name__ == '__main__':
                     # with open('dataset_dict', 'wb') as handle:
                     #     pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
                     if to_record:
-                        with open('vel_data.csv', 'w') as f:
+                        with open('vel_data.csv', 'w',newline='') as f:
                             fields = ['vx', 'vy', 'vz', 'yaw'] 
                             write = csv.writer(f)            
                             write.writerow(fields)
@@ -226,48 +234,58 @@ if __name__ == '__main__':
                     close_env(env_process)
                     exit()
                 elif k == ord('i'):
-                    client.moveByVelocityAsync(0, 0, vz+val, 1)
-                    vx,vy,vz= 0,0,val+vz
+                    # client.moveByVelocityAsync(vx, vy, vz+val, 1)
+                    vx,vy,vz= vx,vy,val+vz
                 elif k == ord('j'):
-                    client.moveByVelocityAsync(0, 0, vz-val, 1)
-                    vx,vy,vz= 0,0,-val+vz
+                    # client.moveByVelocityAsync(0, 0, vz-val, 1)
+                    vx,vy,vz= vx,vy,-val+vz
 
                 elif k == ord('w'):
-                    vx,vy,vz= val+vx,vy,0
-                    action = [vx,vy,vz,-angle*math.pi/180]
-                    do_action(action)
+                    vx,vy,vz= val+vx,vy,vz
+                    # action = [vx,vy,vz,angle*math.pi/180]
+                    # do_action(action)
                     
                 elif k == ord('s'):
-                    vx,vy,vz= -val+vx,vy,0
-                    action = [vx,vy,vz,-angle*math.pi/180]
-                    do_action(action)
+                    vx,vy,vz= -val+vx,vy,vz
+                    # action = [vx,vy,vz,angle*math.pi/180]
+                    # do_action(action)
                     # client.moveByVelocityAsync(-val, 0, 0.0,  1)
                 elif k == ord('a'):
                     vx,vy,vz= vx,-val+vy,0
-                    action = [vx,vy,vz,angle*math.pi/180]
-                    do_action(action)
+                    # action = [vx,vy,vz,angle*math.pi/180]
+                    # do_action(action)
                     # client.moveByVelocityAsync(0.0, val, 0.0, 1)
                 elif k == ord('d'):
                     vx,vy,vz= vx,val+vy,0
-                    action = [vx,vy,vz,angle*math.pi/180]
-                    do_action(action)
+                    # action = [vx,vy,vz,angle*math.pi/180]
+                    # do_action(action)
                     # client.moveByVelocityAsync(0.0, val, 0.0,  1)
                 elif k == ord('e'):
                     # degrees = 20
-                    ya.is_rate = True
+                    # nexus = time.time()
+                    
                     ya.yaw_or_rate+=yaw_rate
-                    client.moveByVelocityAsync(vx, vy, 0.0,  1, drivetrain=0,yaw_mode=ya)
+                    # client.moveByVelocityAsync(vx, vy,vz,  1, drivetrain=0,yaw_mode=ya)
                     # angle+=ya.yaw_or_rate
                 elif k == ord('q'):
                     # angle-=20
-                    ya.is_rate = True
+                    
                     ya.yaw_or_rate-=yaw_rate
-                    client.moveByVelocityAsync(0.0, 0.0, 0.0,  1, drivetrain=0,yaw_mode=ya)
+                    # client.moveByVelocityAsync(vx, vy, vz,  1, drivetrain=0,yaw_mode=ya)
                     # angle-=ya.yaw_or_rate
-                angle-=ya.yaw_or_rate*2*3.14/180
-                angle = angle%360
+                action = [vx,vy,vz,angle*math.pi/180,ya]
+                do_action(action)
+                # angle-=ya.yaw_or_rate
+                # client.moveByVelocityAsync(vx, vy, vz, 1)
+                # angle = angle%360
         except Exception as e:
 
             print(e)
+            if to_record:
+                with open('vel_data.csv', 'w',newline='') as f:
+                    fields = ['vx', 'vy', 'vz', 'yaw'] 
+                    write = csv.writer(f)            
+                    write.writerow(fields)
+                    write.writerows(dataset)
             close_env(env_process)
             print("Closed environment")
